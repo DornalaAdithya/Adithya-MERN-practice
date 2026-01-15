744ec1a051cc15cb8229cab744a3d885bc005197
DornalaAdithya <dornalaadithya@gmail.com>
2026-01-15 17:59:34 +0530

    updated

--- js-assignments/cntrl-stmnts-3.js
+++ js-assignments/cntrl-stmnts-3.js
@@ -1,1 +1,21 @@
-//HANDS-ON 3: Enrollment Eligibility Checker
+/*
+HANDS-ON 3: Enrollment Eligibility Checker
+------------------------------------------
+Initial data:
+    let hasPaid = true;
+    let hasCompletedBasics = false;
+
+Tasks:
+   1. If both conditions are true → "Enroll Now"
+   2. Otherwise → "Complete Requirements"
+   3. Use ternary operator
+   4. Store result in enrollMessage
+   5. Print message
+*/
+
+let hasPaid=true;
+let hasCompletedBasics=false;
+
+let message= (hasPaid && hasCompletedBasics) ? "Enroll Now" : "Complete Requirements";
+
+console.log(message);
